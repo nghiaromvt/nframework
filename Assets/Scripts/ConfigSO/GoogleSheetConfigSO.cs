f9fa48ff7bc4ee01578deafa2e4b57d1b4106ca6
Nghia Nguyen <nghianv@antada.com.vn>
2024-01-15 01:18:24 +0700

    update GoogleSheetConfigSO

--- Assets/Scripts/ConfigSO/GoogleSheetConfigSO.cs
+++ Assets/Scripts/ConfigSO/GoogleSheetConfigSO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,26 +6,35 @@ namespace NFramework
 {
     public class GoogleSheetConfigSO<T> : ScriptableObject where T : new()
     {
-        [SerializeField] private string _sheetId;
-        [SerializeField] private string _gridId;
-        [SerializeField] private string _tsvCachePath;
-        [SerializeField] private string _jsonCachePath;
+        [SerializeField] protected string _sheetId;
+        [SerializeField] protected string _gridId;
+        [SerializeField] protected string _tsvCachePath;
+        [SerializeField] protected string _jsonCachePath;
         [SerializeField] protected List<T> _datas = new List<T>();
 
-#if UNITY_EDITOR
         [ButtonMethod(ButtonMethodAttribute.EDrawOrder.BeforeInspector)]
         public void OpenSheet() => 
             Application.OpenURL($"https://docs.google.com/spreadsheets/d/{_sheetId}/edit#gid={_gridId}");
 
         [ButtonMethod(ButtonMethodAttribute.EDrawOrder.BeforeInspector)]
-        public void Sync() =>
+        protected void Sync() =>
             GoogleSheetHelper.GetConfig<T>(_sheetId, _gridId, OnSynced, _tsvCachePath, _jsonCachePath);
+
+        public void SyncRuntime(Action callback = null)
+        {
+            GoogleSheetHelper.GetConfig<T>(_sheetId, _gridId, data =>
+            {
+                OnSynced(data);
+                callback?.Invoke();
+            });
+        }
 
         protected virtual void OnSynced(List<T> googleSheetData)
         {
             _datas = googleSheetData;
+#if UNITY_EDITOR
             UnityEditor.EditorUtility.SetDirty(this);
-        }
 #endif
+        }
     }
 }
