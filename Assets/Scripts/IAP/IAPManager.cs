8ee3ca84eebd2fc54748cf5a6eabe185cd2dc3e9
Neo <>
2024-01-24 23:11:42 +0700

    add IAP

---
+++ Assets/Scripts/IAP/IAPManager.cs
@@ -0,0 +1,195 @@
+using NFramework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+#if USE_UNITY_PURCHASING
+using UnityEngine.Purchasing;
+using UnityEngine.Purchasing.Extension;
+#endif
+
+public class IAPManager : SingletonMono<IAPManager>
+#if USE_UNITY_PURCHASING
+    , IDetailedStoreListener
+#endif
+{
+#if USE_UNITY_PURCHASING
+    public static event Action<Product> OnPurchased;
+#endif
+
+    [SerializeField] protected List<IAPProductSO> _iapProductSOs = new List<IAPProductSO>();
+
+#if USE_UNITY_PURCHASING
+    private Dictionary<string, IAPProductSO> _idToIAPProductSODic = new Dictionary<string, IAPProductSO>();
+    private IStoreController _controller;
+    private IExtensionProvider _extensions;
+    private bool _isPurchaseInProgress;
+    private Action<bool> _purchaseCallback;
+
+    #region Init
+    public void Init()
+    {
+        if (IsInitialized())
+            return;
+
+        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+        foreach (var iapProductSO in _iapProductSOs)
+        {
+            var id = iapProductSO.id;
+            _idToIAPProductSODic.Add(id, iapProductSO);
+            builder.AddProduct(id, iapProductSO.productType, new IDs()
+            {
+                {iapProductSO.androidProductId, GooglePlay.Name},
+                {iapProductSO.iosProductId, AppleAppStore.Name }
+            });
+        }
+        UnityPurchasing.Initialize(this, builder);
+    }
+
+    private bool IsInitialized() => _controller != null && _extensions != null;
+
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        NFramework.Logger.Log("IAP Initialized", this);
+        _controller = controller;
+        _extensions = extensions;
+
+        foreach (var iapProductSO in _iapProductSOs)
+        {
+            var product = _controller.products.WithID(iapProductSO.id);
+            if (product != null)
+                iapProductSO.PriceString = product.metadata.localizedPriceString;
+        }
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+        NFramework.Logger.Log("IAP init failed");
+        switch (error)
+        {
+            case InitializationFailureReason.AppNotKnown:
+                NFramework.Logger.LogError("Is your App correctly uploaded on the relevant publisher console?", this);
+                break;
+            case InitializationFailureReason.PurchasingUnavailable:
+                // Ask the user if billing is disabled in device settings.
+                NFramework.Logger.Log("Billing disabled!", this);
+                break;
+            case InitializationFailureReason.NoProductsAvailable:
+                // Developer configuration error; check product metadata.
+                NFramework.Logger.Log("No products available for purchase!", this);
+                break;
+        }
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason error, string message) => NFramework.Logger.Log("IAP init failed: " + message, this);
+    #endregion
+
+    #region Purchase
+    public void Purchase(IAPProductSO iapProductSO, Action<bool> callback) => Purchase(iapProductSO.id, callback);
+
+    public void Purchase(string iapProductSO, Action<bool> callback)
+    {
+        if (Application.isEditor || DeviceInfo.IsTestIAP)
+        {
+            callback?.Invoke(true);
+            return;
+        }
+
+        if (_isPurchaseInProgress)
+        {
+            callback?.Invoke(false);
+            NFramework.Logger.Log("Another Purchase in progress", this);
+            return;
+        }
+
+        if (IsInitialized())
+        {
+            var product = _controller.products.WithID(iapProductSO);
+            if (product != null && product.availableToPurchase)
+            {
+                _isPurchaseInProgress = true;
+                _purchaseCallback = callback;
+                ShowLoadingPayment();
+                NFramework.Logger.Log($"Purchasing product asynchronously: '{product.definition.storeSpecificId}'", this);
+                _controller.InitiatePurchase(product);
+            }
+            else
+            {
+                callback?.Invoke(false);
+                NFramework.Logger.Log("BuyProductID: FAIL. Not purchasing product, either is not found or is not available for purchase", this);
+                _isPurchaseInProgress = false;
+            }
+        }
+        else
+        {
+            callback?.Invoke(false);
+            NFramework.Logger.LogError($"Not IsInitialized", this);
+            return;
+        }
+    }
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs purchaseEvent)
+    {
+        var product = purchaseEvent.purchasedProduct;
+        if (_idToIAPProductSODic.TryGetValue(product.definition.id, out var iapProductSO))
+        {
+            if (_isPurchaseInProgress)
+            {
+                NFramework.Logger.Log($"Purchase OK: {product.hasReceipt} {product.transactionID} {product.definition.id}", this);
+                _isPurchaseInProgress = false;
+                HideLoadingPayment();
+                _purchaseCallback?.Invoke(true);
+                _purchaseCallback = null;
+                OnPurchased?.Invoke(product);
+            }
+            else
+            {
+                // We suppose its a restore if the method is called while _isPurchaseInProgress == false
+                NFramework.Logger.Log($"Restoring: {product.hasReceipt} {product.transactionID} {product.definition.id}", this);
+                ProcessRestore(iapProductSO);
+            }
+        }
+        else
+        {
+            NFramework.Logger.LogError($"Cannot find iapProductSO id:{product.definition.id}", this);
+            HideLoadingPayment();
+            _purchaseCallback?.Invoke(true);
+            _purchaseCallback = null;
+        }
+        return PurchaseProcessingResult.Complete;
+    }
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+    {
+        NFramework.Logger.LogError("IAP Purchase Failed: " + failureReason, this);
+        _isPurchaseInProgress = false;
+        HideLoadingPayment();
+        _purchaseCallback?.Invoke(false);
+        _purchaseCallback = null;
+    }
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureDescription failureDescription)
+    {
+        NFramework.Logger.LogError("IAP Purchase Failed: " + failureDescription.message, this);
+        _isPurchaseInProgress = false;
+        HideLoadingPayment();
+        _purchaseCallback?.Invoke(false);
+        _purchaseCallback = null;
+    }
+
+    protected virtual void ProcessRestore(IAPProductSO iapProductSO) { }
+    #endregion
+
+    public IAPProductSO GetProductSO(string iapId)
+    {
+        if (_idToIAPProductSODic.TryGetValue(iapId, out var product))
+            return product;
+
+        NFramework.Logger.LogError($"iapId:{iapId} not found", this);
+        return null;
+    }
+
+    protected virtual void ShowLoadingPayment() => NFramework.Logger.Log("ShowLoadingPayment");
+
+    protected virtual void HideLoadingPayment() => NFramework.Logger.Log("HideLoadingPayment");
+#endif
+}
---
+++ Assets/Scripts/IAP/IAPProductSO.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+namespace NFramework
+{
+    [CreateAssetMenu(menuName = "ScriptableObject/IAPProductSO")]
+    public class IAPProductSO : ScriptableObject
+    {
+        public event Action<string> OnPriceStringChanged;
+
+        public string id;
+        public string androidProductId;
+        public string iosProductId;
+#if USE_UNITY_PURCHASING
+        public UnityEngine.Purchasing.ProductType productType;
+#endif
+        public float defaultPrice;
+
+        private string _priceString;
+        public string PriceString
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_priceString))
+                    return $"{defaultPrice}$";
+                else
+                    return _priceString;
+            }
+            set
+            {
+                _priceString = value;
+                OnPriceStringChanged?.Invoke(value);
+            }
+        }
+    }
+}
+
