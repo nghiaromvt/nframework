54ee3796c60ddfd1e9aa2c7acd6e4ef84ec00cca
Nghia Nguyen <>
2025-03-24 22:37:26 +0700

    clean framework, add packages

--- Assets/Scripts/Singleton/Singleton.cs
+++
@@ -1,9 +0,0 @@
-namespace NFramework
-{
-    public class Singleton<T> where T : class, new()
-    {
-        private static readonly T _i = new T();
-
-        public static T I => _i;
-    }
-}
--- Assets/Scripts/UtilsComponent/DontDestroyOnLoad.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace NFramework
-{
-    public class DontDestroyOnLoad : MonoBehaviour
-    {
-        private void Awake() => DontDestroyOnLoad(gameObject);
-    }
-}
